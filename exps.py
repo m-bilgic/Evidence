98d870ccebfabd3e96e25ab23b781a347174eec0
Mustafa Bilgic <mbilgic@iit.edu>
2014-11-11 11:56:48 -0600

    exps

---
+++ exps.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 11, 2014
+
+@author: mbilgic
+'''
+
+if __name__ == '__main__':
+    # Load the data
+    # Fit a classifier
+    # For the most confident instances
+    #    Compute precision@k using
+    #        Confidence ranking
+    #        Least-conflict ranking
+    # Print the instance that has the most conflict
+    # Print the instance that has the least conflict
+    pass
